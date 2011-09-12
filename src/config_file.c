phatblat/libgit2

commit 932669b8655851cc18ca109e78dfe8d71c0b31c0

Drop STRLEN() macros
There is no need in STRLEN macros. Compilers can do this trivial
optimization on its own.

Signed-off-by: Kirill A. Shutemov <kirill@shutemov.name>

src/config_file.c
@@ -264,7 +264,7 @@ static char *interiorize_section(const char *orig)
 	len = dot - orig;
 	memcpy(section, orig, len);
 	section += len;
-	len = STRLEN(" \"");
+	len = strlen(" \"");
 	memcpy(section, " \"", len);
 	section += len;
 	len = last_dot - dot - 1;

src/commit.c
@@ -225,7 +225,7 @@ int commit_parse_buffer(git_commit *commit, const void *data, size_t len)
 
 	if (git__prefixcmp(buffer, "encoding ") == 0) {
 		const char *encoding_end;
-		buffer += STRLEN("encoding ");
+		buffer += strlen("encoding ");
 
 		encoding_end = buffer;
 		while (encoding_end < buffer_end && *encoding_end != '\n')

src/indexer.c
@@ -176,11 +176,11 @@ static void index_path(char *path, git_indexer *idx)
 
 	ptr = strrchr(path, '/') + 1;
 
-	memcpy(ptr, prefix, STRLEN(prefix));
-	ptr += STRLEN(prefix);
+	memcpy(ptr, prefix, strlen(prefix));
+	ptr += strlen(prefix);
 	git_oid_fmt(ptr, &idx->hash);
 	ptr += GIT_OID_HEXSZ;
-	memcpy(ptr, suffix, STRLEN(suffix));
+	memcpy(ptr, suffix, strlen(suffix));
 }
 
 int git_indexer_write(git_indexer *idx)
@@ -199,7 +199,7 @@ int git_indexer_write(git_indexer *idx)
 
 	namelen = strlen(idx->pack->pack_name);
 	memcpy(filename, idx->pack->pack_name, namelen);
-	memcpy(filename + namelen - STRLEN("pack"), "idx\0", STRLEN("idx\0"));
+	memcpy(filename + namelen - strlen("pack"), "idx", strlen("idx") + 1);
 
 	error = git_filebuf_open(&idx->file, filename, GIT_FILEBUF_HASH_CONTENTS);
 

src/odb_pack.c
@@ -237,7 +237,7 @@ static int packfile_load__cb(void *_data, char *path)
 
 	for (i = 0; i < backend->packs.length; ++i) {
 		struct git_pack_file *p = git_vector_get(&backend->packs, i);
-		if (memcmp(p->pack_name, path, strlen(path) - STRLEN(".idx")) == 0)
+		if (memcmp(p->pack_name, path, strlen(path) - strlen(".idx")) == 0)
 			return GIT_SUCCESS;
 	}
 

src/pack.c
@@ -196,7 +196,7 @@ static int pack_index_open(struct git_pack_file *p)
 		return GIT_SUCCESS;
 
 	idx_name = git__strdup(p->pack_name);
-	strcpy(idx_name + strlen(idx_name) - STRLEN(".pack"), ".idx");
+	strcpy(idx_name + strlen(idx_name) - strlen(".pack"), ".idx");
 
 	error = pack_index_check(idx_name, p);
 	free(idx_name);
@@ -614,7 +614,7 @@ int git_packfile_check(struct git_pack_file **pack_out, const char *path)
 	 * Make sure a corresponding .pack file exists and that
 	 * the index looks sane.
 	 */
-	path_len -= STRLEN(".idx");
+	path_len -= strlen(".idx");
 	if (path_len < 1) {
 		free(p);
 		return git__throw(GIT_ENOTFOUND, "Failed to check packfile. Wrong path name");

src/pkt.c
@@ -267,7 +267,7 @@ int git_pkt_send_flush(int s)
 {
 	char flush[] = "0000";
 
-	return gitno_send(s, flush, STRLEN(flush), 0);
+	return gitno_send(s, flush, strlen(flush), 0);
 }
 
 static int send_want_with_caps(git_remote_head *head, git_transport_caps *caps, int fd)
@@ -279,7 +279,7 @@ static int send_want_with_caps(git_remote_head *head, git_transport_caps *caps, 
 	if (caps->ofs_delta)
 		strcpy(capstr, GIT_CAP_OFS_DELTA);
 
-	len = STRLEN("XXXXwant ") + GIT_OID_HEXSZ + 1 /* NUL */ + strlen(capstr) + 1 /* LF */;
+	len = strlen("XXXXwant ") + GIT_OID_HEXSZ + 1 /* NUL */ + strlen(capstr) + 1 /* LF */;
 	cmd = git__malloc(len + 1);
 	if (cmd == NULL)
 		return GIT_ENOMEM;
@@ -302,10 +302,10 @@ int git_pkt_send_wants(git_headarray *refs, git_transport_caps *caps, int fd)
 {
 	unsigned int i = 0;
 	int error = GIT_SUCCESS;
-	char buf[STRLEN(WANT_PREFIX) + GIT_OID_HEXSZ + 2];
+	char buf[strlen(WANT_PREFIX) + GIT_OID_HEXSZ + 2];
 	git_remote_head *head;
 
-	memcpy(buf, WANT_PREFIX, STRLEN(WANT_PREFIX));
+	memcpy(buf, WANT_PREFIX, strlen(WANT_PREFIX));
 	buf[sizeof(buf) - 2] = '\n';
 	buf[sizeof(buf) - 1] = '\0';
 
@@ -332,8 +332,8 @@ int git_pkt_send_wants(git_headarray *refs, git_transport_caps *caps, int fd)
 		if (head->local)
 			continue;
 
-		git_oid_fmt(buf + STRLEN(WANT_PREFIX), &head->oid);
-		error = gitno_send(fd, buf, STRLEN(buf), 0);
+		git_oid_fmt(buf + strlen(WANT_PREFIX), &head->oid);
+		error = gitno_send(fd, buf, strlen(buf), 0);
 		return git__rethrow(error, "Failed to send want pkt");
 	}
 
@@ -350,13 +350,13 @@ int git_pkt_send_have(git_oid *oid, int fd)
 {
 	char buf[] = "0032have 0000000000000000000000000000000000000000\n";
 
-	git_oid_fmt(buf + STRLEN(HAVE_PREFIX), oid);
-	return gitno_send(fd, buf, STRLEN(buf), 0);
+	git_oid_fmt(buf + strlen(HAVE_PREFIX), oid);
+	return gitno_send(fd, buf, strlen(buf), 0);
 }
 
 int git_pkt_send_done(int fd)
 {
 	char buf[] = "0009done\n";
 
-	return gitno_send(fd, buf, STRLEN(buf), 0);
+	return gitno_send(fd, buf, strlen(buf), 0);
 }

src/remote.c
@@ -93,7 +93,7 @@ int git_remote_get(git_remote **out, git_config *cfg, const char *name)
 	}
 
 	/* "fetch" is the longest var name we're interested in */
-	buf_len = STRLEN("remote.") + STRLEN(".fetch") + strlen(name) + 1;
+	buf_len = strlen("remote.") + strlen(".fetch") + strlen(name) + 1;
 	buf = git__malloc(buf_len);
 	if (buf == NULL) {
 		error = GIT_ENOMEM;

src/repository.c
@@ -449,12 +449,12 @@ static int read_gitfile(char *path_out, const char *file_path, const char *base_
 	for (;data[end_offset] == '\r' || data[end_offset] == '\n'; --end_offset);
 	data[end_offset + 1] = '\0';
 
-	if (STRLEN(GIT_FILE_CONTENT_PREFIX) == end_offset + 1) {
+	if (strlen(GIT_FILE_CONTENT_PREFIX) == end_offset + 1) {
 		git_futils_freebuffer(&file);
 		return git__throw(GIT_ENOTFOUND, "No path in git file `%s`", file_path);
 	}
 
-	data = data + STRLEN(GIT_FILE_CONTENT_PREFIX);
+	data = data + strlen(GIT_FILE_CONTENT_PREFIX);
 	error = git_path_prettify_dir(path_out, data, base_path);
 	git_futils_freebuffer(&file);
 

commit 312ee530e3ef322eaa34a79d8bd8bda6d3c16c14

Fix a bug and GCC warning introduced in 932669b
For unsigned types, the comparison >= 0 is always true, so avoid it by using
a post-decrement and integrating the initial assigment into the loop body.
No change in behavior is intended.

src/config_file.c
@@ -553,10 +553,8 @@ static char *cfg_readline(diskfile_backend *cfg)
 
 	memcpy(line, line_src, line_len);
 
-	line[line_len] = '\0';
-
-	while (--line_len >= 0 && isspace(line[line_len]))
-		line[line_len] = '\0';
+	do line[line_len] = '\0';
+	while (line_len-- > 0 && isspace(line[line_len]));
 
 	if (*line_end == '\n')
 		line_end++;