phatblat/libgit2

commit a667ca8298193b3103c1dbdcb1f6c527e6e99eb2

Change the git_branch_iterator_new and git_branch_next definitions to use git_branch_t.

include/git2/branch.h
@@ -83,7 +83,7 @@ typedef struct git_branch_iterator git_branch_iterator;
 GIT_EXTERN(int) git_branch_iterator_new(
 	git_branch_iterator **out,
 	git_repository *repo,
-	unsigned int list_flags);
+	git_branch_t list_flags);
 
 /**
  * Retrieve the next branch from the iterator
@@ -93,7 +93,7 @@ GIT_EXTERN(int) git_branch_iterator_new(
  * @param iter the branch iterator
  * @return 0 on success, GIT_ITEROVER if there are no more branches or an error code.
  */
-GIT_EXTERN(int) git_branch_next(git_reference **out, unsigned int *out_type, git_branch_iterator *iter);
+GIT_EXTERN(int) git_branch_next(git_reference **out, git_branch_t *out_type, git_branch_iterator *iter);
 
 /**
  * Free a branch iterator

tests-clar/refs/branches/iterator.c
@@ -33,7 +33,7 @@ static void assert_retrieval(unsigned int flags, unsigned int expected_count)
 	git_branch_iterator *iter;
 	git_reference *ref;
 	int count = 0, error;
-	unsigned int type;
+	git_branch_t type;
 
 	cl_git_pass(git_branch_iterator_new(&iter, repo, flags));
 	while ((error = git_branch_next(&ref, &type, iter)) == 0) {
@@ -83,7 +83,7 @@ static void assert_branch_has_been_found(struct expectations *findings, const ch
 static void contains_branches(struct expectations exp[], git_branch_iterator *iter)
 {
 	git_reference *ref;
-	unsigned int type;
+	git_branch_t type;
 	int error, pos = 0;
 
 	while ((error = git_branch_next(&ref, &type, iter)) == 0) {

src/branch.c
@@ -129,7 +129,7 @@ typedef struct {
 	unsigned int flags;
 } branch_iter;
 
-int git_branch_next(git_reference **out, unsigned int *out_type, git_branch_iterator *_iter)
+int git_branch_next(git_reference **out, git_branch_t *out_type, git_branch_iterator *_iter)
 {
 	branch_iter *iter = (branch_iter *) _iter;
 	git_reference *ref;
@@ -159,7 +159,7 @@ int git_branch_next(git_reference **out, unsigned int *out_type, git_branch_iter
 int git_branch_iterator_new(
 	git_branch_iterator **out,
 	git_repository *repo,
-	unsigned int list_flags)
+	git_branch_t list_flags)
 {
 	branch_iter *iter;
 

tests-clar/online/push.c
@@ -182,7 +182,7 @@ static void verify_tracking_branches(git_remote *remote, expected_ref expected_r
 	char *actual_ref;
 	git_oid oid;
 	int failed = 0, error;
-	unsigned int branch_type;
+	git_branch_t branch_type;
 	git_reference *ref;
 
 	/* Get current remote branches */

commit 0fe48557c8f717ae481c4a45871655668fd379b5

Add GIT_BRANCH_ALL to git_branch_t enum
git_branch_t is an enum so requesting GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE is not possible as it is not a member of the enum (at least VS2013 C++ complains about it).

This fixes a regression introduced in commit a667ca8298193b3103c1dbdcb1f6c527e6e99eb2 (PR #1946).

Signed-off-by: Sven Strickroth <email@cs-ware.de>

include/git2/branch.h
@@ -84,7 +84,7 @@ typedef struct git_branch_iterator git_branch_iterator;
  * @param repo Repository where to find the branches.
  * @param list_flags Filtering flags for the branch
  * listing. Valid values are GIT_BRANCH_LOCAL, GIT_BRANCH_REMOTE
- * or a combination of the two.
+ * or GIT_BRANCH_ALL.
  *
  * @return 0 on success  or an error code
  */

include/git2/types.h
@@ -193,6 +193,7 @@ typedef enum {
 typedef enum {
 	GIT_BRANCH_LOCAL = 1,
 	GIT_BRANCH_REMOTE = 2,
+	GIT_BRANCH_ALL = GIT_BRANCH_LOCAL|GIT_BRANCH_REMOTE,
 } git_branch_t;
 
 /** Valid modes for index and tree entries. */

tests/refs/branches/iterator.c
@@ -48,7 +48,7 @@ static void assert_retrieval(unsigned int flags, unsigned int expected_count)
 
 void test_refs_branches_iterator__retrieve_all_branches(void)
 {
-	assert_retrieval(GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE, 14);
+	assert_retrieval(GIT_BRANCH_ALL, 14);
 }
 
 void test_refs_branches_iterator__retrieve_remote_branches(void)
@@ -139,7 +139,7 @@ void test_refs_branches_iterator__mix_of_packed_and_loose(void)
 
 	r2 = cl_git_sandbox_init("testrepo2");
 
-	cl_git_pass(git_branch_iterator_new(&iter, r2, GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE));
+	cl_git_pass(git_branch_iterator_new(&iter, r2, GIT_BRANCH_ALL));
 	contains_branches(exp, iter);
 
 	git_branch_iterator_free(iter);